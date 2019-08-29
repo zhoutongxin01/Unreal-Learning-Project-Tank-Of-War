c244379937b665340058387786ee40d601c90b0b
zhoutongxin <zhoutongxin@mtv.com>
2019-08-29 10:13:43 +0800

    Creat Project

---
+++ Source/MyTankOfWar/MyTankOfWar.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyTankOfWar.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyTankOfWar, "MyTankOfWar" );
---
+++ Source/MyTankOfWar/MyTankOfWarGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyTankOfWarGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTANKOFWAR_API AMyTankOfWarGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
